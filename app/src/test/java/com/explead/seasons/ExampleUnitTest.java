3691bcd4c8078415a481dba880b4cf50f96acf8c
teeeses <teeeses@mail.ru>
2019-04-21 14:44:57 +0300

    Unit test для spref

--- app/src/test/java/com/explead/seasons/ExampleUnitTest.java
+++ app/src/test/java/com/explead/seasons/ExampleUnitTest.java
@@ -2,6 +2,10 @@ package com.explead.seasons;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,7 +15,32 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void test1() throws Exception {
+        assertEquals(getArrayFromString(""), new ArrayList<Integer>());
+    }
+
+    @Test
+    public void test2() throws Exception {
+        assertEquals(getArrayFromString("1"), new ArrayList<>(Arrays.asList(1)));
+    }
+
+    @Test
+    public void test3() throws Exception {
+        assertEquals(getArrayFromString("2|3"), new ArrayList<>(Arrays.asList(2, 3)));
+    }
+
+    @Test
+    public void test4() throws Exception {
+        assertEquals(getArrayFromString("2|6|23|56"), new ArrayList<>(Arrays.asList(2, 6, 23, 56)));
+    }
+
+    private ArrayList<Integer> getArrayFromString(String str) {
+        String replaceStr = str.replaceAll("[^0-9]+", " ");
+        ArrayList<Integer> array = new ArrayList<>();
+        String[] list = replaceStr.trim().split(" ");
+        for (String item : list) {
+            array.add(Integer.parseInt(item));
+        }
+        return array;
     }
 }
